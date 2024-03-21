3ed5b96e798891242b6fc6fef0df4f26cd1f484f
Alfin89 <rebahansantuy30@gmail.com>
2024-03-22 00:37:28 +0700

    Menambahkan frontend react Js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        Home
+      </div>
+    </>
+  )
+}
+
+export default App
