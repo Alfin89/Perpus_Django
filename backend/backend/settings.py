0831e2af82df371e2839a0bc3a0bda7ab5af4a33
Alfin89 <rebahansantuy30@gmail.com>
2024-03-23 03:21:39 +0700

    menambahkan CORS agar mendapatkan izin untuk mengambil data dari backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -84,6 +86,11 @@ DATABASES = {
     }
 }
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import axios from 'axios'
 
 function App() {
+  const [buku, setBuku] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://127.0.0.1:8000/api/buku/');
+      const data = response.data;
+      setBuku(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
-        Home
+      {buku.map((buku, index) => (
+        <div key={index}>
+          <h2>{buku.judul_buku}</h2>
+          <p>{buku.pengarang}</p>
+          <p>{buku.jenis_buku}</p>
+          <img src={buku.cover} alt="Gambar" style={{ width: '200px', height: 'auto' }} />
+        </div>
+      ))}
       </div>
     </>
   )
